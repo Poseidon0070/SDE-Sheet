fed9325ab05f48a211b0e2fb0e2111e0daea464d
Shikhar <criticalxsynergy@gmail.com>
2024-06-17 00:11:51 +0530

    SDE sheet

---
+++ SegmentTree/1.cpp
@@ -0,0 +1,16 @@
+// In segment tree we divide range into lesser number of nodes(max 2*logn) constructing a tree of height log n, where n is the size of array
+// updating involves going to root of that corresponding index and then upliffting & updating upper nodes simultaneously
+
+// In segment tree we need to think about : 
+// 1.) node structure      -> state
+// 2.) how to merge?       -> transition
+// 3.) Leaf nodes
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,q; 
+    cin >> n >> q;
+    return 0;
+}
